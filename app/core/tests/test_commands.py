80d3d495407b65e9ed792b9023f3cf5b3eb1717e
Samuel <samuel@adivare.nl>
2023-01-26 16:55:44 +0100

    Nosense

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -3,7 +3,7 @@ Test custom Django management commands.
 """
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2OpError
+from psycopg2 import OperationalError as Psycopg2Error
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
@@ -13,6 +13,7 @@ from django.test import SimpleTestCase
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands."""
+
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for the database to be ready."""
         patched_check.return_value = True
@@ -22,9 +23,8 @@ class CommandTests(SimpleTestCase):
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting  for database when getting OperationalError."""
-        patched_check.side_effect = [Psycopg2OpError] * 2 + \
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
                                     [OperationalError] * 3 + [True]
         call_command('wait_for_db')
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
-
